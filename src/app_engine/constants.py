95e75368346fdfb713ca7bf8aa8dabd4cc6de911
unknown <jinxixing210@gmail.com>
2018-05-09 11:11:50 +0800

    app

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -11,7 +11,7 @@ REDIRECT_DOMAINS =  [
   'rtc.intopi.com'
 ]
 # URL which we should redirect to if matching in REDIRECT_DOMAINS.
-REDIRECT_URL = 'http://rtc.intopi.com'
+REDIRECT_URL = 'rtc.intopi.com'
 
 ROOM_MEMCACHE_EXPIRATION_SEC = 60 * 60 * 24
 MEMCACHE_RETRY_LIMIT = 100
@@ -25,66 +25,64 @@ LOOPBACK_CLIENT_ID = 'LOOPBACK_CLIENT_ID'
 ICE_SERVER_OVERRIDE  = [
   {
     "urls": [
-      "turn:hostname/rtc.intopi.com:3478?transport=udp",
-      "turn:hostname/rtc.intopi.com:3478?transport=tcp"
+      "turn:hostname/69.79.26.13:3478?transport=udp",
+      "turn:hostname/69.79.26.13:3478?transport=tcp"
     ],
     "username": "webrtc",
     "credential": "@yb254f67u!"
   },
   {
     "urls": [
-      "stun:hostname/rtc.intopi.com:3478"
+      "stun:69.79.26.13:3478"
     ]
   }
 ]
-TURN_BASE_URL = 'http://rtc.intopi.com'
 
 ICE_SERVER_BASE_URL = 'http://rtc.intopi.com'
 ICE_SERVER_URL_TEMPLATE = '%s/v1alpha/iceconfig?key=%s'
 ICE_SERVER_API_KEY = os.environ.get('ICE_SERVER_API_KEY')
 
 # Dictionary keys in the collider instance info constant.
-WSS_INSTANCE_HOST_KEY = 'rtc.intopi.com:8089'
+WSS_INSTANCE_HOST_KEY = '69.79.26.13:8089'
 WSS_INSTANCE_NAME_KEY = 'vm_name'
 WSS_INSTANCE_ZONE_KEY = 'zone'
 WSS_INSTANCES = [{
-    WSS_INSTANCE_HOST_KEY: 'rtc.intopi.com:8089',
+    WSS_INSTANCE_HOST_KEY: '69.79.26.13:8089',
     WSS_INSTANCE_NAME_KEY: 'wsserver-std',
     WSS_INSTANCE_ZONE_KEY: 'us-central1-a'
-},{
-    WSS_INSTANCE_HOST_KEY: 'rtc.intopi.com:8089',
+}, {
+    WSS_INSTANCE_HOST_KEY: '69.79.26.13:8089',
     WSS_INSTANCE_NAME_KEY: 'wsserver-std-2',
     WSS_INSTANCE_ZONE_KEY: 'us-central1-f'
-}
-]
+}]
 
-# WSS_HOST_PORT_PAIRS = [ins[WSS_INSTANCE_HOST_KEY] for ins in WSS_INSTANCES]
+WSS_HOST_PORT_PAIRS = [ins[WSS_INSTANCE_HOST_KEY] for ins in WSS_INSTANCES]
 
-# # memcache key for the active collider host.
-# WSS_HOST_ACTIVE_HOST_KEY = 'rtc.intopi.com:3478'
+# memcache key for the active collider host.
+WSS_HOST_ACTIVE_HOST_KEY = 'wss_host_active_host'
 
-# # Dictionary keys in the collider probing result.
-# WSS_HOST_IS_UP_KEY = 'is_up'
-# WSS_HOST_STATUS_CODE_KEY = 'status_code'
-# WSS_HOST_ERROR_MESSAGE_KEY = 'error_message'
+# Dictionary keys in the collider probing result.
+WSS_HOST_IS_UP_KEY = 'is_up'
+WSS_HOST_STATUS_CODE_KEY = 'status_code'
+WSS_HOST_ERROR_MESSAGE_KEY = 'error_message'
 
-# RESPONSE_ERROR = 'ERROR'
-# RESPONSE_ROOM_FULL = 'FULL'
-# RESPONSE_UNKNOWN_ROOM = 'UNKNOWN_ROOM'
-# RESPONSE_UNKNOWN_CLIENT = 'UNKNOWN_CLIENT'
-# RESPONSE_DUPLICATE_CLIENT = 'DUPLICATE_CLIENT'
-# RESPONSE_SUCCESS = 'SUCCESS'
-# RESPONSE_INVALID_REQUEST = 'INVALID_REQUEST'
+RESPONSE_ERROR = 'ERROR'
+RESPONSE_ROOM_FULL = 'FULL'
+RESPONSE_UNKNOWN_ROOM = 'UNKNOWN_ROOM'
+RESPONSE_UNKNOWN_CLIENT = 'UNKNOWN_CLIENT'
+RESPONSE_DUPLICATE_CLIENT = 'DUPLICATE_CLIENT'
+RESPONSE_SUCCESS = 'SUCCESS'
+RESPONSE_INVALID_REQUEST = 'INVALID_REQUEST'
 
-# IS_DEV_SERVER = os.environ.get('APPLICATION_ID', '').startswith('dev')
+IS_DEV_SERVER = os.environ.get('APPLICATION_ID', '').startswith('dev')
 
-# BIGQUERY_URL = 'https://www.googleapis.com/auth/bigquery'
+BIGQUERY_URL = 'https://www.googleapis.com/auth/bigquery'
 
-# # Dataset used in production.
-# BIGQUERY_DATASET_PROD = 'prod'
+# Dataset used in production.
+BIGQUERY_DATASET_PROD = 'prod'
 
-# # Dataset used when running locally.
-# BIGQUERY_DATASET_LOCAL = 'dev'
+# Dataset used when running locally.
+BIGQUERY_DATASET_LOCAL = 'dev'
 
-# # BigQuery table within the dataset.
-# BIGQUERY_TABLE = 'analytics'
+# BigQuery table within the dataset.
+BIGQUERY_TABLE = 'analytics'
